0ce85311354e4c306e3231191fffe07f6a380d1b
Kev <gfk002003@gmail.com>
2025-07-17 18:36:27 +0200

    Petites modif des liens => a continer

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-
+from flask import render_template, request, redirect, url_for
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
