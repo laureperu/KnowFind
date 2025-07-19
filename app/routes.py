81cc531e2f908e946c21bf650c7ccc80c8086788
laureperu <laure.peru@telecom-sudparis.eu>
2025-07-19 14:06:51 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,6 @@ from flask import render_template, request, redirect, url_for
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/upload_rex')
+def upload_rex():
+    return render_template('upload_rex.html')
